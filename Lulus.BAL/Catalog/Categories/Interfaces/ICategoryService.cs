4ed4672959e640ab3924fefeede8d77041299eef
Linh Mac Le My <67138256+Maruhara18774@users.noreply.github.com>
2021-07-13 12:03:04 +0700

    First service

---
+++ Lulus.BAL/Catalog/Categories/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lulus.BAL.Catalog.Categories.DTOs;
+
+namespace Lulus.BAL.Catalog.Categories.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryViewModel>> GetAllCategory();
+    }
+}
---
+++ Lulus.BackendApi/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Lulus.BAL.Catalog.Categories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lulus.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var categories = await _categoryService.GetAllCategory();
+            return Ok(categories);
+        }
+    }
+}
